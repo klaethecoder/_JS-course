c38bd08916b274e6595504743feae0fa656e4a28
Ryan Richard <ryanrichard@Ryans-MacBook-Pro.local>
2020-02-10 20:30:59 -0700

    Created a function that would help to make sure that the uncompleted todos were sorted to the top.

--- _arrays/todo.js
+++ _arrays/todo.js
@@ -1,6 +1,9 @@
-const todos = [{text:"Wash the car", completed: false}, {text:'Take out the trash', completed: false}, {text:'Get Gas', completed: true}, 
+const todos = [{text:"Wash the car", completed: true}, {text:'Take out the trash', completed: false}, {text:'Get Gas', completed: true}, 
 {text:'Buy Groceries', completed: false}, 
 {text:'Do homework', completed: true}];
+
+sortTodos(todos)
+console.log(todos)
 
 console.log(`You have ${todos.length} todos`)
 console.log(todos[0], todos[todos.length -2])
@@ -47,4 +50,18 @@ function uncompleted(todos){
     return todos.filter((todo)=> !todo.completed)
 }
 
-console.log(uncompleted(todos))
+// console.log(uncompleted(todos))
+
+
+// Section 6: Video 45(See for reference.)
+function sortTodos(arr){
+    arr.sort((a,b)=> {
+        if(!a.completed && b.completed){
+            return -1
+        } else if(!b.completed && a.completed){
+            return 1
+        } else{
+            return 0
+        }
+    })
+}
