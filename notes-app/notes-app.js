c849865c08eab60f7a26d0199a0a3250e4f3f527
Ryan Richard <ryanrichard@Ryans-MacBook-Pro.local>
2020-02-11 04:55:52 -0700

    I added the notes that were created in the Array folder and added them to the Notes JS file

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,3 +1,14 @@
+const notes = [{
+    title:'Note 1',
+    body: 'This is a note that is for the demonstration'}, {
+    title:'Note 2',
+    body: 'Parting is such sweet sorrow'}, {
+    title: 'Note 3',
+    body: 'Wakanda Forever'},
+    {
+      title: 'Nope',
+      body: 'Lets get it started'}];
+
 console.log('This is from an external doc.')
 
 // let p = document.querySelector('p')
@@ -8,3 +19,7 @@ ps.forEach(function(el){
     console.log(el)
     el.remove()
 });
+
+let newP = document.createElement('p')
+newP.textContent = "This is a new paragraph element from JS."
+document.body.appendChild(newP)
